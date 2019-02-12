679140e02b5d4bed00e89c1f07e90bbc28bbe220
Pierre Nallet <pierrenallet@hotmail.com>
2019-02-12 11:44:27 -0500

    adds google stuff

--- WebApp2/Controllers/HomeController.cs
+++ WebApp2/Controllers/HomeController.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 
@@ -8,11 +10,18 @@ namespace WebApp2.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index(string id)
+        public async Task<ActionResult> Index(string id)
         {
             if (id == "bug")
             {
                 throw new Exception("you asked for it");
+            }
+            if (id == "google")
+            {
+                var httpClient = new HttpClient();
+                var result = await httpClient.GetAsync("http://google.com");
+                var s = await result.Content.ReadAsStringAsync();
+                return Content(s);
             }
             return View();
         }
